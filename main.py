fff674cdc95d1b5bf60da9b1ea76c27b3e48ec7f
Jakub Zielinski <jakub.zieli02@gmail.com>
2023-04-04 18:32:09 +0200

    Dynamic HTML uses jsonplaceholder data

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import get_content_func
+
 
 app = FastAPI()
 
@@ -12,3 +14,22 @@ templates = Jinja2Templates(directory="Frontend")
 @app.get("/")
 async def read_item(request: Request):
     return templates.TemplateResponse("index.html", context= {"request": request})
+
+
+
+@app.get("/posty/{test}", response_class=HTMLResponse)
+def write_variable(request: Request, test: int):
+
+    
+    test = get_content_func.get_post()[test-1]["body"]
+
+    return templates.TemplateResponse("DisplayPosts.html",{"request":request, "test":test} )
+
+
+@app.get("/photo/{photoID}", response_class=HTMLResponse)
+def write_variable(request: Request, photoID: int):
+
+    
+    photoID = get_content_func.get_photos()[photoID-1]["url"]
+
+    return templates.TemplateResponse("DisplayPhotos.html",{"request":request, "photoID":photoID} )
