b8a59312491d7be5be02c95575c6bb389f6dfb6d
Jakub Zielinski <jakub.zieli02@gmail.com>
2023-04-18 16:41:44 +0200

    moved files into different directories

---
+++ backend/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import Backend.get_content_func as get_content_func
+
+
+app = FastAPI()
+
+app.mount("/Frontend", StaticFiles(directory="frontend"), name="Frontend")
+
+templates = Jinja2Templates(directory="frontend")
+
+@app.get("/")
+async def read_item(request: Request):
+    return templates.TemplateResponse("index.html", context= {"request": request})
+
+
+
+@app.get("/posty/{test}", response_class=HTMLResponse)
+def write_variable(request: Request, test: int):
+
+    
+    test = get_content_func.get_post()[test-1]["body"]
+
+    return templates.TemplateResponse("DisplayPosts.html",{"request":request, "test":test} )
+
+
+@app.get("/photo/{photoID}", response_class=HTMLResponse)
+def write_variable(request: Request, photoID: int):
+
+    
+    photoID = get_content_func.get_photos()[photoID-1]["url"]
+
+    return templates.TemplateResponse("DisplayPhotos.html",{"request":request, "photoID":photoID} )
